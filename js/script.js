9919d90295f6e862460452a33c45429ce9b2eb7e
flo_h <flo.1244@gmail.com>
2021-10-01 12:52:45 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+const clockTime = function(){
+	
+   const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+	//console.log(seconds);
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+	//console.log(mins);
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+	//console.log(hour);
+	
+};
+
+
+setInterval(clockTime, 1000);
+clockTime();
